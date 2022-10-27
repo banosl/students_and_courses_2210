754ccda53ab33813a7e2e79b7425847543852d42
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-27 09:19:30 -0600

    Test: wrote test for student.name

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -4,7 +4,14 @@ RSpec.describe Student do
 
  it 'student exists' do
   student = Student.new({name: "Morgan", age: 21})
+  
   expect(student).to be_instance_of(Student)
+ end
+
+ it 'student has a name' do
+  student = Student.new({name: "Morgan", age: 21})
+  
+  expect(student.name).to eq("Morgan")
  end
 
 end
