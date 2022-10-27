2b3fdf96638151959a014cc83edb8f5d802ce78c
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-27 09:51:56 -0600

    Test: wrote tests for course name and course capacity

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -7,5 +7,16 @@ RSpec.describe Course do
 
   expect(course).to be_instance_of(Course)
  end
+ 
+ it 'course has a name' do
+  course = Course.new("Calculus", 2)
+  
+  expect(course.name).to eq("Calculus")
+ end
 
+ it 'course has a capacity' do
+  course = Course.new("Calculus", 2)
+  
+  expect(course.capacity).to eq(2)
+ end
 end
