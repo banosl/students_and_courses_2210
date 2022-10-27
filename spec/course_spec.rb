44a062163be143b7dbc6ae7565fb35fbf0f7c413
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-27 10:13:40 -0600

    Test: wrote test for #enroll(student)

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -33,4 +33,18 @@ RSpec.describe Course do
    expect(course.full?).to be(false)
   end
  end
+
+ describe '#enroll(student)' do
+  it 'course can enrolls students' do
+    course = Course.new("Calculus", 2)
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+
+    course.enroll(student1)
+    course.enroll(student2)
+
+    expect(course.students).to eq([student1, student2])
+    expect(course.full?).to be(true)
+  end
+ end
 end
