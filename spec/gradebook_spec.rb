d6903619ad7dfdf2c937b18e14a7404d6db6fc11
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-27 10:25:30 -0600

    Test: wrote test for #instructor

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -4,8 +4,14 @@ require './lib/student'
 
 RSpec.describe Gradebook do
  it 'gradebook exists'do
-  gradebook = Gradebook.new(instructor)
+  gradebook = Gradebook.new("Mx Alexander")
 
   expect(gradebook).to be_instance_of(Gradebook)
+ end
+
+ it 'gradebook has an instructor' do
+  gradebook = Gradebook.new("Mx Alexander")
+
+  expect(gradebook.instructor).to eq("Mx Alexander")
  end
 end
