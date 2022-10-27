05ffee8029b25ea0ad47dcfe4591011af545353d
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-27 10:54:17 -0600

    Test: started writing a test for ##students_below_2.5

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -46,4 +46,20 @@ RSpec.describe Gradebook do
     expect(gradebook.students_in_courses).to eq(course1.students)
   end
  end
+
+ describe '#students_below_2.5' do
+  it 'gradebook can list students that have a grade 2.5 or lower in their course' do
+    gradebook = Gradebook.new("Mx Alexander")
+    course1 = Course.new("Calculus", 2)
+    gradebook.add_courses(course1)
+
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+
+    course1.enroll(student1)
+    course1.enroll(student2)
+
+    expect(gradebook.)
+  end 
+ end
 end
