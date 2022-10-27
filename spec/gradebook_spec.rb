e752062542e099f44b8c8f46a0f5b6db6e04014d
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-27 10:39:13 -0600

    Test: wrote test for #students_in_courses

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -22,12 +22,28 @@ RSpec.describe Gradebook do
  end
 
  describe '#add_courses' do
-  it 'gradebook can add courses' do
+  it 'gradebook can add courses and can list them' do
     gradebook = Gradebook.new("Mx Alexander")
     course1 = Course.new("Calculus", 2)
     gradebook.add_courses(course1)
 
     expect(gradebook.courses).to eq([course1])
+  end
+ end
+ 
+ describe '#students_in_courses' do
+  it 'gradebook can list the students added to a course' do
+    gradebook = Gradebook.new("Mx Alexander")
+    course1 = Course.new("Calculus", 2)
+    gradebook.add_courses(course1)
+
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+
+    course1.enroll(student1)
+    course1.enroll(student2)
+
+    expect(gradebook.students_in_courses).to eq(course1.students)
   end
  end
 end
