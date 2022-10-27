daf8269d794290e966d1c921bf5c55e2d6b0652b
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-27 09:05:03 -0600

    Test: wrote test on Student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,10 @@
 require './lib/student'
 
 RSpec.describe Student do
+
+ it 'student exists' do
+  student = Student.new({name: "Morgan", age: 21})
+  expect(student).to be_instance_of(Student)
+ end
+
 end
